dda058698c6e664351423174ba1d7bc9b6955215
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-19 12:58:22 +0100

    PR

--- course/frontend/control-flow/scripts/main.js
+++ course/frontend/control-flow/scripts/main.js
@@ -1,34 +1,36 @@
-console.log("============================ ES5 =============================")
-// function _largerInteger(num1, num2) {
-//     if (num1 > num2)
-//         return num1;
+function _largerInteger(num1, num2) {
+    if (num1 > num2)
+        return num1;
 
-//     return num2;
-// }
+    return num2;
+}
+
 function largerInteger(num1, num2) {
     return num1 > num2 ? num1 : num2;
 }
+
 console.log('largerInteger should result 56 for inputs -34 and 56 =>', largerInteger(-34, 56));
 console.log('largerInteger should result -34 for inputs -34 and -56 =>', largerInteger(-34, -56));
 
-// function _signProduct() {
-//     var isPositive = true;
-//     for (var i = 0; i < arguments.length; i++) {
-//         if (arguments[i] < 0) {
-//             isPositive = !isPositive;
-//         };
-//     };
-//     return "The sign is " + (isPositive ? "+" : "-");
-// };
+function _signProduct() {
+    var isPositive = true;
+    for (var i = 0; i < arguments.length; i++) {
+        if (arguments[i] < 0) {
+            isPositive = !isPositive;
+        };
+    };
+    return "The sign is " + (isPositive ? "+" : "-");
+};
 
-// function _signProduct(...numArgs) {
-//     var nums = numArgs
-//     var boo = true
-//     for (let i = 0; i < numArgs.length; i++) {
-//         numArgs[i] < 0 ? boo = !boo : boo
-//     }
-//     return `The sign is ${boo ? ` +` : ` -`}`
-// }
+function _signProduct(...numArgs) {
+    var nums = numArgs
+    var boo = true
+    for (let i = 0; i < numArgs.length; i++) {
+        numArgs[i] < 0 ? boo = !boo : boo
+    }
+    return `The sign is ${boo ? ` +` : ` -`}`
+}
+
 function signProduct(a, b, c) {
     if (a * b * c > 0) {
         return ('The sign is +');
@@ -36,93 +38,117 @@ function signProduct(a, b, c) {
         return ('The sign is -');
     }
 }
-console.log('signProduct should result "-" for inputs 1, -2, 3 =>', signProduct(1, -2, 3));
-console.log('signProduct should result "+" for inputs 1, -2, -3 =>', signProduct(1, -2, -3));
-console.log('signProduct should result "-" for inputs -1, -2, -3 =>', signProduct(-1, -2, -3));
 
-// function sortNumbers(v1, v2, v3) {
-//     var num1 = v1;
-//     var num2 = v2;
-//     var num3 = v3;
-//     if (v1 < v2) {
-//         num1 = v1;
-//         num2 = v2;
-//     } else if (v1 > v2) {
-//         num1 = v2;
-//         num2 = v1;
-//     }
-//     var num21, num22, num23;
-//     if (v3 > num2) {
-//         num21 = num1;
-//         num22 = num2;
-//         num23 = v3;
-//     } else if (v3 < num2 && v3 > num1) {
-//         num21 = num1;
-//         num22 = v3;
-//         num23 = num2;
-//     } else if (v3 < num1) {
-//         num21 = v3;
-//         num22 = num1;
-//         num23 = num2;
-//     }
-//     var message = num23 + ', ' + num22 + ', ' + num21;
-//     return message;
-// }
-// function _sortNumbers(num1, num2, num3) {
-//     if (num1 > num2 && num2 > num3) {
-//         return [num1, num2, num3];
-//     } else if (num2 > num1 && num1 > num3) {
-//         return [num2, num1, num3];
-//     } else if (num3 > num1 && num1 > num2) {
-//         return [num3, num1, num2];
-//     } else if (num1 > num3 && num3 > num2) {
-//         return [num1, num3, num2];
-//     } else if (num2 > num3 && num3 > num1) {
-//         return [num2, num3, num1];
-//     } else if (num3 > num2 && num2 > num1) {
-//         return [num3, num2, num1];
-//     }
-// }
-// function _sortNumbers(num1, num2, num3) {
-//     if (num1 > num2 && num1 > num3 && num2 > num3)
-//         return num1 + " " + num2 + " " + num3;
-//     else if (num2 > num1 && num2 > num3 && num1 > num3)
-//         return num2 + " " + num1 + " " + num3;
-//     else if (num3 > num1 && num3 > num2 && num1 > num2)
-//         return num3 + " " + num1 + " " + num2;
-//     else if (num1 > num2 && num1 > num3 && num2 < num3)
-//         return num1 + " " + num3 + " " + num2;
-// }
-function sortNumbers(a, b, c) {
+console.log('signProduct should result "-" for 1, -2, 3 =>', signProduct(1, -2, 3));
+console.log('signProduct should result "+" for 1, -2, -3 =>', signProduct(1, -2, -3));
+console.log('signProduct should result "-" for -1, -2, -3 =>', signProduct(-1, -2, -3));
+//console.log('signProduct should result "+" for -1, -2, -3, -4, 5 =>', signProduct(-1, -2, -3, -4, 5));
+
+function sortNumbers(v1, v2, v3) {
+    var num1 = v1;
+    var num2 = v2;
+    var num3 = v3;
+    if (v1 < v2) {
+        num1 = v1;
+        num2 = v2;
+    } else if (v1 > v2) {
+        num1 = v2;
+        num2 = v1;
+    }
+    var num21, num22, num23;
+    if (v3 > num2) {
+        num21 = num1;
+        num22 = num2;
+        num23 = v3;
+    } else if (v3 < num2 && v3 > num1) {
+        num21 = num1;
+        num22 = v3;
+        num23 = num2;
+    } else if (v3 < num1) {
+        num21 = v3;
+        num22 = num1;
+        num23 = num2;
+    }
+    var message = num23 + ', ' + num22 + ', ' + num21;
+    return message;
+}
+
+function _sortNumbers(a, b, c) {
     var array = [];
     switch (true) {
-        case a > b && b > c: array.push(a, b, c); break;
-        case b > c && c > a: array.push(b, c, a); break;
-        case c > a && a > b: array.push(c, a, b); break;
-        case a > c && c > b: array.push(a, c, b); break;
-        case b > a && a > c: array.push(b, a, c); break;
-        case c > b && b > a: array.push(c, b, a); break;
-        default: console.error("unpredicted case!");
+      case a > b && b > c: //FIX: correct expressions! ex: a > b && b > c
+        array.push(a, b, c);
+        break;
+      case b > c && c > a:
+        array.push(b, c, a);
+        break;
+      case c > a && a > b:
+        array.push(c, a, b);
+        break;
+      case a > c && c > b:
+        array.push(a, c, b);
+        break;
+      case b > a && a > c:
+        array.push(b, a, c); //FIX: This push array
+        break;
+      case c > b && b > a: //FIX: This push array
+        array.push(c, b, a);
+        break;
+      default:
+        console.error("unpredicted case!");
+      /*
+                sortNumbers should result 2, 15, -13 for input 15, 2, -13 => Output: 2, 15, -13, 
+                sortNumbers should result -2, -15, -13 for input -13, -15, -2 => Output: -13, -15, -2, 
+            */
     }
-    // var string = '';
-    // for (var i = 0; i <= array.length - 1; i++) {
-    //     string = string + array[i] + ', '
-    // }
-    // return "Output: " + string;
-    return `Output : ${array.join(',')}`
+    var string = '';
+    for (var i = 0; i <= array.length - 1; i++) {
+        string = string + array[i] + ', '
+    }
+    return "Output: " + string;
 }
-console.log('sortNumbers should result -1,-2,-3 for inputs -2, -1, -3 =>', sortNumbers(-2, -1, -3));
-console.log('sortNumbers should result 10,6,-3 for inputs -3, 10, 6 =>', sortNumbers(-3, 10, 6));
 
-// function _oddOrEven() {
-//     var response = "";
-//     for (var i = 0; i <= 15; i++) {
-//         response += i + " is " + (i % 2 ? "odd" : "even") + "\n"
-//     };
-//     return response;
-// }
+function _sortNumbers(num1, num2, num3) {
+    if (num1 > num2 && num2 > num3) {
+        return [num1, num2, num3];
+    } else if (num2 > num1 && num1 > num3) {
+        return [num2, num1, num3];
+    } else if (num3 > num1 && num1 > num2) {
+        return [num3, num1, num2];
+    } else if (num1 > num3 && num3 > num2) {
+        return [num1, num3, num2];
+    } else if (num2 > num3 && num3 > num1) {
+        return [num2, num3, num1];
+    } else if (num3 > num2 && num2 > num1) {
+        return [num3, num2, num1];
+    }
+}
 
-function oddOrEvent(a) {
+function _sortNumbers(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3 && num2 > num3)
+        return num1 + " " + num2 + " " + num3;
+    else if (num2 > num1 && num2 > num3 && num1 > num3)
+        return num2 + " " + num1 + " " + num3;
+    else if (num3 > num1 && num3 > num2 && num1 > num2)
+        return num3 + " " + num1 + " " + num2;
+    else if (num1 > num2 && num1 > num3 && num2 < num3)
+        return num1 + " " + num3 + " " + num2;
+}
+
+
+console.log("sortNumbers should result 4, 0, -1 for input 0, -1, 4 =>", sortNumbers(0, -1, 4));
+console.log("sortNumbers should result 2, 15, -13 for input 15, 2, -13 =>", sortNumbers(2, 15, -13));
+console.log("sortNumbers should result -2, -15, -13 for input -13, -15, -2 =>", sortNumbers(-13, -15, -2));
+
+function _oddOrEven() {
+    var response = "";
+    for (var i = 0; i <= 15; i++) {
+        response += i + " is " + (i % 2 ? "odd" : "even") + "\n"
+    };
+    return response;
+}
+
+function oddOrEven(a) {
     var res = '';
     var b = -1;
     while (++b <= a) {
@@ -134,26 +160,28 @@ function oddOrEvent(a) {
     }
     return res;
 }
-console.log("oddOrEvent should return odd and even values of 15 (separated) =>", oddOrEvent(15));
 
-// function largestNumber(v1, v2, v3, v4, v5) {
-//     var largest;
-//     if (v1 > v2) {
-//         largest = v1;
-//     } else {
-//         largest = v2;
-//     }
-//     if (v3 > largest) {
-//         largest = v3;
-//     }
-//     if (v4 > largest) {
-//         largest = v4;
-//     }
-//     if (v5 > largest) {
-//         largest = v5;
-//     }
-//     return largest;
-// }
+console.log("oddOrEvent should return odd and even values of 15 (separated) =>", oddOrEven(15));
+
+function largestNumber(v1, v2, v3, v4, v5) {
+    var largest;
+    if (v1 > v2) {
+        largest = v1;
+    } else {
+        largest = v2;
+    }
+    if (v3 > largest) {
+        largest = v3;
+    }
+    if (v4 > largest) {
+        largest = v4;
+    }
+    if (v5 > largest) {
+        largest = v5;
+    }
+    return largest;
+}
+
 function largestNumber(a) {
     var swapp;
     var n = a.length - 1;
@@ -172,38 +200,9 @@ function largestNumber(a) {
     } while (swapp);
     return x[0];
 }
-console.log('largestNumber for inputs -2, -1, -3 should result -1=>', largestNumber([-2, -1, -3]));
-console.log('largestNumber for inputs -2, 0, -3, 67, 89, 3, 1 should result 68=>', largestNumber([-2, 0, -3, 67, 89, 3, 1]));
-console.log('largestNumber for inputs 0, 1, -3, 2, 0, 4, 78 should result 78=>', largestNumber([0, 1, -3, 2, 0, 4, 78]));
 
-function fizzBuzz() {
-    var result = ''
-    for (var i = 1; i <= 100; i++) {
-        if ((i % 3 == 0) && (i % 5 == 0)) result += "FizzBuzz\n"
-        else if (i % 3 == 0) result += "Fizz\n"
-        else if (i % 5 == 0) result += "Buzz\n"
-        else result += `${i}\n`
-    }
-    return result
-}
-console.log('fizzBuzz should result 1, 2, Fizz, 4, Buzz, ... , 14, FizzBuzz, 16, ...=>\n', fizzBuzz());
 
-// ### contruct_pattern
-function asterisct() {
-    var result = ''
-    for (var i = 1; i < 8; i++) {
-        result += Array(i).join("*") + "\n"
-    }
-    return result
-}
-console.log('Tendria que mostrar un arbolito muy bonito!! =>\n', asterisct());
-
-function oddEvent (a){
-    var sum = 0;
-    while(a--) {
-        if (a%3 == 0) sum += a
-        else if (a%5 == 0) sum += a
-    }
-    return sum
-}
-console.log("Should return a sum of all 3 and 5 multiples: " + oddEven(1000))
+//console.log("largestNumber(-5, -2, -6, 0, -1) should return the largest of input numbers =>", largestNumber(-5, -2, -6, 0, -1));
+//console.log("largestNumber(5, -2, 6, 100, -1) should return the largest of input numbers =>", largestNumber(5, -2, 6, 100, -1));
+console.log("largestNumber([-5, -2, -6, 0, -1]) should return the largest of input numbers =>", largestNumber([-5, -2, -6, 0, -1]));
+console.log("largestNumber([5, -2, 6, 100, -1]) should return the largest of input numbers =>", largestNumber([5, -2, 6, 100, -1]));
--- course/frontend/hangman/scripts/Hangman.js
+++ course/frontend/hangman/scripts/Hangman.js
@@ -1,10 +1,169 @@
-// function Hangman(word, attemps) {
-//     // TODO
-// }
+function _Hangman(word, attemps) {
+    var cadena = [];
+    for (var i = 0; i < word.length; i++) {
+        cadena.push('_');
+    }
+    var play = true;
+    var ok = false;
+    this.try = function (charOrWord) {
+        var message = '';
+        if (play) {
+            if (charOrWord.length <= 1) {
+                // QUERY all this logic could be processed in one function?
+                for (var i = 0; i < word.length; i++) {
+                    if (charOrWord.toLowerCase() === word.charAt(i).toLowerCase()) {
+                        ok = true;
+                        // cadena.splice(i, 1, _word.toUpperCase());
+                        cadena[i] = charOrWord.toUpperCase();
+                    }
+                }
+                if (ok) {
+                    (cadena.indexOf('_') === -1) ?
+                    (message = cadena.join(" ") + ' // Very Well you acerted', play = false) :
+                    (ok = false, message = attemps + " " + cadena.join(" "));
+                } else {
+                    --attemps;
+                    (attemps === 0) ?
+                    (message = 'Sorry, you have not guessed... the correct word is ' + word.toUpperCase() + '.', play = false) :
+                    (message = attemps + " " + cadena.join(" "));
+                }
+            } else {
+                // QUERY all this logic could be processed in one function?
+                play = false;
+                return (charOrWord === word ? 'You have guessed the word, well done!' : 'Sorry, you have not guessed... the correct word is ' + word.toUpperCase() + '.');
+            }
+        } else {
+            message = 'GAME OVER.';
+        }
+        return message;
+    }
+}
 
-// Hangman.prototype.try = function(charOrWord) {
-//     // TODO
-// };
+function _Hangman(word, attemps) {
+    var wordArr = word.toUpperCase().split('');
+    var N = word.length;
+    var board = [];
+    for (var i = 0; i < N; i++) {
+        board[i] = '_';
+    }
+    var letterCount = 0;
+    var gameOver = false;
+    var win = false;
+
+    this.attemps = attemps;
+
+    this.try = function (response) {
+        if (!gameOver && !win) {
+            if (!response || typeof response !== "string") {
+                return "Please, enter a valid character or word!";
+            }
+            var responseUpper = response.toUpperCase();
+            if (responseUpper.length > 1) {
+                // QUERY all this logic could be processed in one function?
+                //palabra
+                if (responseUpper === word.toUpperCase()) {
+                    //win
+                    win = true;
+                    return "You have guessed the word, well done!";
+                } else {
+                    //lose
+                    gameOver = true;
+                    return "Sorry, you have not guessed... the correct word is " + word.toUpperCase() + ".";
+                }
+            } else {
+                // QUERY all this logic could be processed in one function?
+                //letra
+                var foundALetter = false;
+                for (var i = 0; i < N; i++) {
+                    if (responseUpper === wordArr[i]) {
+                        board[i] = wordArr[i];
+                        //delete wordArr[i];
+                        wordArr[i] = undefined;
+                        letterCount++
+                        foundALetter = true;
+                    }
+                }
+                // mostramos tablero
+                if (!foundALetter) {
+                    attemps--;
+                }
+                if (attemps === 0) {
+                    gameOver = true;
+                }
+                if (N === letterCount) {
+                    win = true;
+                }
+                return attemps + ' ' + board.join(' ');
+            }
+        } else {
+            //Show message
+            return "GAME OVER.";
+        }
+    }
+}
+
+function _Hangman(word, attempts) {
+    var w = word.split('');
+    var underscore = new Array(w.length);
+    var endGame = false;
+    for (var i = 0; i < underscore.length; i++)
+        underscore[i] = '_';
+
+    function win(charOrWord) {
+        if (charOrWord === word || underscore.join('') === word)
+            return true;
+    }
+
+    function gameOver(charOrWord) {
+        if (attempts === 0) {
+            return true;
+        } else if (charOrWord.length > 1 && charOrWord != word) {
+            return true;
+        }
+
+    }
+    this.try = function (charOrWord) {
+        if (endGame === false) {
+            var check = false;
+            for (var i = 0; i < w.length; i++) {
+                if (charOrWord === w[i]) {
+                    underscore[i] = w[i].toUpperCase();
+                    check = true;
+                }
+            }
+            if (check === true) {
+                check = false;
+                if (win(charOrWord)) {
+                    endGame = true;
+                    return 'You have guessed the word, well done!';
+                } else {
+                    return attempts + ' ' + underscore.join(' ');
+                }
+            } else if (win(charOrWord)) {
+                endGame = true;
+                return 'You have guessed the word, well done!';
+            } else {
+                attempts--;
+                if (gameOver(charOrWord)) {
+                    if (charOrWord.length > 1) {
+                        endGame = true;
+                        return 'Sorry, you have not guessed... the correct word is HELLO.';
+                    } else {
+                        endGame = true;
+                        return 'Sorry, you have not guessed... the correct word is HELLO.';
+                    }
+                } else {
+                    return attempts + ' ' + underscore.join(' ');
+                }
+
+            }
+        } else {
+            return 'GAME OVER.'
+        }
+
+    }
+}
+
 function Hangman(word, attemps) {
     var word = word.toLocaleLowerCase()
     if (word && attemps) {
@@ -13,17 +172,13 @@ function Hangman(word, attemps) {
             var self = this
             if (string && isNaN(string)) {
                 attemps--
-
                 if (attemps >= 0) return (string.length > 1) ? allWord(string) : letter(string)
-
                 return "GAME OVER."
             }
-
             return console.error("no letter inputed")
             //Clousure
             function letter(string) {
                 var string = string.toLocaleLowerCase()
-
                 if (word.includes(string)) {
                     attemps++
                     var currentWordUser = word.split("").map(function (el) {
@@ -37,7 +192,6 @@ function Hangman(word, attemps) {
                     console.log("Nice! " + string + " is correct letter! ")
                     return attemps + " " + self.print()
                 }
-
                 console.log("Nop... Try again...")
                 return attemps + " " + self.print()
             }
@@ -53,7 +207,6 @@ function Hangman(word, attemps) {
         }
         this.print = function () {
             var arrWor = word.split("")
-
             return arrWor.map(function (el) {
                 return (correctLetters.includes(el)) ? el.toUpperCase() : "_"
             }).join(" ")
@@ -62,44 +215,3 @@ function Hangman(word, attemps) {
         return console.error("no word or attemps inputed")
     }
 }
-
-
-
-function toNormalCase(string) {
-    return string.split('').map(el => (el <= 'Z') ? ` ${el.toLowerCase()}` : el).join('')
-}
-
-function random(arr) {
-    var count = 0
-    var randomArr
-    console.log(count);
-    (function structure() {
-        randomArr = arr.map(el => el[Math.floor(el.length * Math.random())])
-    })()
-
-    return randomArr.filter((a, b, c) => {
-        try {
-            count ++
-            c.forEach(function (el) {
-                console.log(el,a)
-                if (el === a) throw BreakException;
-            });
-            return true
-        } catch (e) {
-           random(arr)
-        }
-    })
-}
-
-
-
-// arr.filter((a, b, c) => {
-//     try {
-//         c.forEach(function (el) {
-//             if (el === a) throw BreakException;
-//         });
-//         return true
-//     } catch (e) {
-//        random(err)
-//     }
-// })
