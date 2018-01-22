8a50ce70629cab4e6c5abb779e4ed4e62b4fddd7
JaimeRC <jaime.rc@icloud.com>
2018-01-22 14:29:36 +0100

    add camelCase

--- course/frontend/camel-case-to-lower-case-with-spaces/scripts/main.js
+++ course/frontend/camel-case-to-lower-case-with-spaces/scripts/main.js
@@ -1,1 +1,6 @@
-// TODO
+function camelCase(text) {
+    var regEx = /[A-Z\d]+/g;
+    text = text.replace(regEx, function (n) { return ' ' + n; });
+    return text;
+}
+camelCase('hangmanResultsWellDoneWhenAssertingWordOnThe7thAttempt');
