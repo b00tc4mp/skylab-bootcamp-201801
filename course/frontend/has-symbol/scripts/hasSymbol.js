706e0cae42a7abe9ed28d09d8266373f96bcb208
Edgar Ayllon <edgar.ayllon@outlook.es>
2018-01-23 08:33:03 +0100

    feature/has-symbol

--- course/frontend/has-symbol/scripts/hasSymbol.js
+++ course/frontend/has-symbol/scripts/hasSymbol.js
@@ -1,9 +1,8 @@
 'use strict'
 function hasSymbol( _string ){
 
-	if ( typeof _string !== 'string' ||  ( _string.length > 1  || _string.length === 0 ) ||
-		 (/[\s\n\t]/g).test( _string) ) return 'ERROR: OUT of SPECS' ;
+	if ( typeof _string !== 'string' ||   _string.length === 0  || (/[\n\t]/g).test( _string) )  return 'ERROR: OUT of SPECS' ;
 
-	var re = /[^\d\wñç]/ig ;
+	var re = /[^\s\d\wñç]/ig ;
 	return ( re.exec( _string ) !== null );
 };
