77358452050d79ba81d1fba1f0b24b6392e9df3f
Dídac García <didaquis@gmail.com>
2018-01-23 17:33:53 +0100

    Moved code

--- course/frontend/polyfills/scripts/main.js
+++ course/frontend/polyfills/scripts/main.js
@@ -11,23 +11,3 @@ a.forEach(function(v) {
 var s = 'hola mundo';
 
 console.log(s.singleQuotes());
-
-
-
-// Another example:
-/*
-   * String.prototype.hashtag
-   *
-   * @returns {String} Formated string like hashtag.
-*/
-String.prototype.hashtag = function(){
-	var text = this.trim();
-	text = text.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-	text = text.replace(/\s/g, '');
-	return '#' + text;
-}
-
-var s = 'Skylab rocks';
-console.log( s.hashtag() ); // SkylabRocks
-
-console.log( "esto es un ejemplo".hashtag() ); // #EstoEsUnEjemplo
