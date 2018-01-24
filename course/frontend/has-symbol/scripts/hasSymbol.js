dac732da519f264970b3f054e4c00c75f458e231
E.Ayllon <34253437+EdgarAyllon@users.noreply.github.com>
2018-01-24 21:51:24 +0100

    Feature/has-symbol
    Implemented try..catch method , fixed name conventions to accomplish standards.

--- course/frontend/has-symbol/scripts/hasSymbol.js
+++ course/frontend/has-symbol/scripts/hasSymbol.js
@@ -2,9 +2,11 @@
 // RETURN true if any, otherwise false.
 'use strict'
 function hasSymbol( _string ){
-
-	if ( typeof _string !== 'string' ||	 _string.length === 0 || (/[\n\t]/g).test( _string) ) return 'ERROR: OUT of SPECS' ;
-
+	try{
+		if( typeof _string !== 'string' || _string.length === 0 ||	(/[\n\t]/g).test( _string) )  throw new Error(e);
+	}catch(e){
+		return 'ERROR: OUT of SPECS' ;
+	}
 	var re = /[^\d\w\sñç]/ig ;
 	return ( re.exec( _string ) !== null );
 };
