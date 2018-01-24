b421eed83ffc99086e11f855357b2cc0dafd604a
Alex Gonzalez <alexgonrod83@gmail.com>
2018-01-24 17:47:40 +0100

    Arrays exercices 20, 21

--- course/frontend/arrays/scripts/main.js
+++ course/frontend/arrays/scripts/main.js
@@ -1,96 +1,141 @@
-function isArray(arr) {
-    return arr instanceof Array;
-}
+// function isArray(arr) {
+//     return arr instanceof Array;
+// }
 
-function _isArray(arr) {
-    return Array.isArray(arr);
-}
+// function _isArray(arr) {
+//     return Array.isArray(arr);
+// }
 
-function _isArray(a) {
-    // TODO make this impl null / undefined safe!
-    return a.constructor == Array
-}
+// function _isArray(a) {
+//     // TODO make this impl null / undefined safe!
+//     return a.constructor == Array
+// }
 
-function _isArray(array) {
-    // TODO make this impl null / undefined safe!
-    // WARN! this impl also passes tests 6 and 7, but shouldn't!
-    return array.length >= 0 && ((typeof array) == "object");
-}
+// function _isArray(array) {
+//     // TODO make this impl null / undefined safe!
+//     // WARN! this impl also passes tests 6 and 7, but shouldn't!
+//     return array.length >= 0 && ((typeof array) == "object");
+// }
 
-function _isArray(element) {
-    if (element === null || element === undefined) {
-        return false;
-    }
-    var result = Object.prototype.toString.call(element).slice(8, -1);
-    return (result == "Array") ? true : false;
-}
+// function _isArray(element) {
+//     if (element === null || element === undefined) {
+//         return false;
+//     }
+//     var result = Object.prototype.toString.call(element).slice(8, -1);
+//     return (result == "Array") ? true : false;
+// }
 
-console.log('0) isArray(undefined) should result false', isArray(undefined));
-console.log('0.1) isArray(null) should result false', isArray(null));
-console.log('1) isArray([]) should result true', isArray([]));
-console.log('2) isArray([1, 2, 3]) should result true', isArray([1, 2, 3]));
-console.log('3) isArray("string") should result false', isArray('string'));
-console.log('4) isArray({}) should result false', isArray({}));
-console.log('5) isArray(1) should result false', isArray(1));
-console.log('6) isArray({ length: 0 }) should result false', isArray({
-    length: 0
-}));
-console.log('7) isArray({ length: 1 }) should result false', isArray({
-    length: 1
-}));
+// console.log('0) isArray(undefined) should result false', isArray(undefined));
+// console.log('0.1) isArray(null) should result false', isArray(null));
+// console.log('1) isArray([]) should result true', isArray([]));
+// console.log('2) isArray([1, 2, 3]) should result true', isArray([1, 2, 3]));
+// console.log('3) isArray("string") should result false', isArray('string'));
+// console.log('4) isArray({}) should result false', isArray({}));
+// console.log('5) isArray(1) should result false', isArray(1));
+// console.log('6) isArray({ length: 0 }) should result false', isArray({
+//     length: 0
+// }));
+// console.log('7) isArray({ length: 1 }) should result false', isArray({
+//     length: 1
+// }));
 
-function _joinElements(arr) {
-    // TODO make this impl null / undefined safe!
-    var output = '';
-    for (var i = 0; i <= arr.length - 1; i++) {
-        if (i == arr.length - 1) {
-            output += arr[i];
-        } else {
-            output += arr[i] + ', ';
+// function _joinElements(arr) {
+//     // TODO make this impl null / undefined safe!
+//     var output = '';
+//     for (var i = 0; i <= arr.length - 1; i++) {
+//         if (i == arr.length - 1) {
+//             output += arr[i];
+//         } else {
+//             output += arr[i] + ', ';
+//         }
+//     }
+//     return output;
+// }
+
+// function joinElements(a) {
+//     // TODO make this impl null / undefined safe!
+//     return a.join(',');
+// }
+
+// //console.log('0) joinElements(undefined) should result joined elements', joinElements(undefined));
+// console.log('1) joinElements([1, 2, 3]) should result joined elements', joinElements([1, 2, 3]));
+
+// function _arrayClone(a) {
+//     // ERROR! do not pass the tests! should return a new array copy of the original!
+//     return new Array(a)
+// }
+
+// function arrayClone(arr) {
+//     return arr.slice();
+// }
+
+// function _arrayClone(array) {
+//     // INFO same as before, but more extensive.
+//     return array.slice(0, array.length);
+// }
+
+// var arr = [1, 2, 3];
+// var res;
+// console.log('1) arrayClone([1, 2, 3])', res = arrayClone(arr), 'and should be a new array', res != arr);
+// arr = [1, 2, 3, [1, 2, 3]];
+// console.log('1) arrayClone([1, 2, 3, [1, 2, 3]])', res = arrayClone(arr), 'and should be a new array', res != arr);
+
+// function recursiveArrayClone(source) {
+//     var result = [];
+//     for (var i = 0; i < source.length; i++) {
+//         if (isArray(source[i])) {
+//             // Ejemplo de recursividad
+//             result.push(recursiveArrayClone(source[i]));
+//         } else {
+//             result.push(source[i]);
+//         }
+//     }
+//     return result;
+// }
+
+// console.log('1) recursiveArrayClone([1, 2, 3, [1, 2, 3]])', res = recursiveArrayClone(arr), 'and should be a new array', res != arr, 'and sub-array should be a new array', arr[3] != res[3]);
+
+
+//Alex GR
+
+//removeDuplicate
+var duplicate = [];
+
+function duplicates(arr) {
+    for (var i = 0; i < arr.length; i++) {
+
+        if (duplicate.indexOf(arr[i]) == -1) {
+
+            duplicate.push(arr[i]);
         }
+
     }
-    return output;
+    return duplicate;
 }
+duplicates(["Alice", "John", "Maria", "John"])
 
-function joinElements(a) {
-    // TODO make this impl null / undefined safe!
-    return a.join(',');
-}
 
-//console.log('0) joinElements(undefined) should result joined elements', joinElements(undefined));
-console.log('1) joinElements([1, 2, 3]) should result joined elements', joinElements([1, 2, 3]));
+//showChoice
+function showChoice(arr) {
+    var chain = '';
+    for (var i = 0; i < arr.length; i++) {
+        switch (i + 1) {
+            case 1:
+                chain += (i + 1) + 'st choice is ' + arr[i] + '\n';
+                break;
+            case 2:
+                chain += (i + 1) + 'nd choice is ' + arr[i] + '\n';
+                break;
+            case 3:
+                chain += (i + 1) + 'rd choice is ' + arr[i] + '\n';
+                break;
 
-function _arrayClone(a) {
-    // ERROR! do not pass the tests! should return a new array copy of the original!
-    return new Array(a)
-}
-
-function arrayClone(arr) {
-    return arr.slice();
-}
-
-function _arrayClone(array) {
-    // INFO same as before, but more extensive.
-    return array.slice(0, array.length);
-}
-
-var arr = [1, 2, 3];
-var res;
-console.log('1) arrayClone([1, 2, 3])', res = arrayClone(arr), 'and should be a new array', res != arr);
-arr = [1, 2, 3, [1, 2, 3]];
-console.log('1) arrayClone([1, 2, 3, [1, 2, 3]])', res = arrayClone(arr), 'and should be a new array', res != arr);
-
-function recursiveArrayClone(source) {
-    var result = [];
-    for (var i = 0; i < source.length; i++) {
-        if (isArray(source[i])) {
-            // Ejemplo de recursividad
-            result.push(recursiveArrayClone(source[i]));
-        } else {
-            result.push(source[i]);
+            default:
+                chain += (i + 1) + 'th choice is ' + arr[i] + '\n';
+                break;
         }
-    }
-    return result;
-}
 
-console.log('1) recursiveArrayClone([1, 2, 3, [1, 2, 3]])', res = recursiveArrayClone(arr), 'and should be a new array', res != arr, 'and sub-array should be a new array', arr[3] != res[3]);
+    }
+    return chain;
+}
+console.log(showChoice(["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "]))
