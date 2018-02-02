71c88ad3c9f2b6090e22b6f77dafb99a90192e02
Carlos Ramos <31883557+Asix94@users.noreply.github.com>
2018-02-02 11:29:20 +0100

    include function doOnChars

---
+++ text-tool/scripts/text-tool-2.0.0b.js
@@ -0,0 +1,40 @@
+/**
+ * Text tool
+ * 
+ * @version 2.0.0
+ */
+
+var text;
+(function () {
+  function wrap(text, before, after) {
+    return (before || '') + text + (after || before || '');
+  }
+
+  function doOnChars(text,operationOnChars) {
+    let newText = '';
+    for (let index in text) {
+      newText += operationOnChars(text[index]);
+    }
+    return newText;
+  }
+
+  function charInBrackets(char) {
+    return char !== " " ? "[" + char + "]" : char;
+  }
+
+  text = function (_text) {
+    var __text = new String(_text);
+
+    __text.wrap = function (before, after) {
+      return text(wrap(_text, before, after));
+    }
+
+    __text.doOnChars = function () {
+      return text(doOnChars(_text,charInBrackets));
+    }
+
+    return __text;
+  }
+})();
+
+console.log(text('something').wrap('$').wrap('[', ']').wrap('{', '}').wrap('<', '>').wrap('#').doOnChars().toString())
