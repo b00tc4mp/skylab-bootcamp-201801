99107ed51975915a6ed1883b4ba853286b5c60d2
manuelbarzi <manuelbarzi@gmail.com>
2018-02-02 17:35:04 +0100

    update stuff

--- es6/promises/scripts/main.js
+++ es6/promises/scripts/main.js
@@ -1,3 +1,5 @@
+// custom promise demo
+
 promise(function () {
     console.log('START async task...')
 
@@ -10,3 +12,108 @@ promise(function () {
     .catch(err => console.error(err))
 
 console.log('CONTINUE doing other stuff...')
+
+// Promise
+
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('---')
+
+        try {
+            // DO something
+
+            // throw new Error('ERROR on doing something')
+
+            resolve('RUN me async')
+        } catch (err) {
+            // throw err // WARN! UNCONTROLLED error!
+
+            // reject(err)
+        }
+
+    }, 1000)
+})
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+
+console.log('CONTINUE beyond p...')
+
+// chaining promises
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('---')
+
+        try {
+            // DO something
+
+            // throw new Error('ERROR on doing something')
+
+            resolve('RUN me async p2')
+        } catch (err) {
+            // throw err // WARN! UNCONTROLLED error!
+
+            reject(err)
+        }
+
+    }, 500)
+})
+    .then(res => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('---')
+
+            try {
+                // DO something
+
+                throw new Error('ERROR on doing something')
+
+                resolve(res + ' / RUN me async p2-2')
+            } catch (err) {
+                // throw err // WARN! UNCONTROLLED error!
+
+                reject(err)
+            }
+
+        }, 500)
+    }))
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+
+console.log('CONTINUE beyond p2...')
+
+// promise with multiple thens
+
+const p3 = new Promise((resolve, reject) => {
+    resolve('RUN me async p3')
+})
+    .then(res => res + ' / RUN me async p3-2')
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+
+console.log('CONTINUE beyond p3...')
+
+// math about promise thens chained
+
+const calc = new Promise((resolve, reject) => {
+    resolve(1)
+})
+    .then(res => res + 1)
+    .then(res => res * 2)
+    .then(res => res / 3)
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+
+console.log('CONTINUE beyond calc...')
+
+// concat about promise thens chained
+
+const concat = new Promise((resolve, reject) => {
+    resolve('a')
+})
+    .then(res => res + 'b')
+    .then(res => res + 'c')
+    .then(res => res + 'd')
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+
+console.log('CONTINUE beyond concat...')
