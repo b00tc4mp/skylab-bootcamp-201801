60bdd53788b9edae1c127005dd790d5f4116bc6e
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-04 18:02:38 +0100

    :pencil2: adding simple return on templates.js

--- hangman-es6/vic-hangman-es6/scripts/template/inGame-template.js
+++ hangman-es6/vic-hangman-es6/scripts/template/inGame-template.js
@@ -1,10 +1,9 @@
 function generateInGameTemplate(attemps, print) {
-    let inGameTemplate = `
+    return `
     <form class='inGame'>
         <h1 id="myStatusTitleWord">${attemps}) ${print}</h1>
         <input id="inputInGame" type="text">
         <button type="submit">Try...</button>
     </form>
     `
-    return inGameTemplate;
 }
--- hangman-es6/vic-hangman-es6/scripts/template/init-template.js
+++ hangman-es6/vic-hangman-es6/scripts/template/init-template.js
@@ -1,10 +1,9 @@
 function generateInitTemplate() {
-    let initTemplate = `
+    return `
     <form class='initGame'>
         <h1>Insert a word to play in HangPredator</h1>
         <input id="inputInitWordGame" type="text">
         <button type="submit">DONE!</button>
     </form>
     `
-    return initTemplate;
 }
