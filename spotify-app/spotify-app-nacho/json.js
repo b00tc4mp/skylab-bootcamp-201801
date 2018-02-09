27cf145cdd3abb6e6484b966141d42a57557c414
mediacloner <alexbcn@gmail.com>
2018-02-09 14:16:50 +0100

    update

---
+++ spotify-app/spotify-app-nacho/json.js
@@ -0,0 +1,16 @@
+var o =[
+    {
+        id:"8OucfG",
+        name:"Mahou Maestra",
+        nameDisplay:"Mahou Maestra",
+        abv:"7.5",
+        styleId:97,
+        isOrganic:"N",
+        labels:{
+            icon:"https://s3.amazonaws.com/brewerydbapi/beer/8OucfG/upload_z4mgUk-icon.png",
+            medium:"https://s3.amazonaws.com/brewerydbapi/beer/8OucfG/upload_z4mgUk-medium.png",
+            large:"https://s3.amazonaws.com/brewerydbapi/beer/8OucfG/upload_z4mgUk-large.png"
+        },
+        status:"verified"
+    }
+];
