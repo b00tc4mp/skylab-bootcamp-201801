9ebeac7b104af0e697927e289574cbe12b2ca3a7
Víctor Gámez <victor.gamez1596@gmail.com>
2018-02-27 09:16:00 +0100

    fibonacci generator

--- staff/vic/es6-generators/generator.js
+++ staff/vic/es6-generators/generator.js
@@ -10,3 +10,22 @@ function* idMaker(){
   console.log(gen.next().value); // 1
   console.log(gen.next().value); // 2
   console.log(gen.next().value); // undefined
+
+//====== OTHER EXAMPLE ======\\
+
+function* fibonacciMaker(){
+    let a=0, b=1
+    
+    while (true) {
+        let f = a
+        a = b
+        b = f + a
+        let reset = yield f
+        if (reset) a = 0, b = 1
+    }
+}
+
+const fibo = fibonacciMaker()
+
+fibo.next()
+fibo.next(true) //reset fibonacci
