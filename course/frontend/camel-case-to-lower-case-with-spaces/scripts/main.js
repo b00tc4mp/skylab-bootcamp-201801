59044ddefeedde03180aa5fdb0cb9782f0c0b213
JaimeRC <jaime.rc@icloud.com>
2018-02-27 15:40:49 +0100

    delete

--- course/frontend/camel-case-to-lower-case-with-spaces/scripts/main.js
+++
@@ -1,6 +0,0 @@
-function camelCase(text) {
-    var regEx = /[A-Z\d]+/g;
-    text = text.replace(regEx, function (n) { return ' ' + n; });
-    return text;
-}
-camelCase('hangmanResultsWellDoneWhenAssertingWordOnThe7thAttempt');
