849906d28afdf62ecb41e45ad60658a0fef345e4
mediacloner <alexbcn@gmail.com>
2018-02-27 12:51:34 +0100

    update

--- stuff/node/pug/task-app/src/index.js
+++ stuff/node/pug/task-app/src/index.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const taskLogic = require('./taskLogic')
 
 const app = express()
 
-const tasks = []
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
     //tasks.push({ id: 1, text: 'hola mundo!', done: false})
 
-    res.render('index', { tasks })
+    res.render('index', { todos: taskLogic.listTodo(), dones: taskLogic.listDone() })
 })
 
 const formBodyParser = bodyParser.urlencoded({ extended: false })
@@ -20,13 +20,7 @@ const formBodyParser = bodyParser.urlencoded({ extended: false })
 app.post('/tasks', formBodyParser, (req, res) => {
     const { body: { text } } = req
 
-    const task = {
-        id: new Date().getTime(),
-        text,
-        done: false
-    }
-
-    tasks.push(task)
+    taskLogic.create(text)
 
     res.redirect('/')
 })
@@ -34,9 +28,7 @@ app.post('/tasks', formBodyParser, (req, res) => {
 app.get('/tasks/:id/done', (req, res) => {
     const {  params: { id } } = req
 
-    const task = tasks.find(task => task.id == id)
-
-    if (task) task.done = true
+    taskLogic.markDone(id)
 
     res.redirect('/')
 })
@@ -44,9 +36,7 @@ app.get('/tasks/:id/done', (req, res) => {
 app.get('/tasks/:id/remove', (req, res) => {
     const {  params: { id } } = req
 
-    const index = tasks.findIndex(task => task.id == id)
-
-    if (index > -1) tasks.splice(index, 1)
+    taskLogic.removeDone(id)
 
     res.redirect('/')
 })
