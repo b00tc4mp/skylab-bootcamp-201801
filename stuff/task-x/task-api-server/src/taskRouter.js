f6ede1f0f6137c6f97509f4578d24b24a6927721
manuelbarzi <manuelbarzi@gmail.com>
2018-02-28 10:22:23 +0100

    update stuff

---
+++ stuff/task-x/task-api-server/src/taskRouter.js
@@ -0,0 +1,86 @@
+const { Router } = require('express')
+const bodyParser = require('body-parser')
+const { success, fail } = require('./api-utils')
+
+const { TaskLogic } = require('task-logic')
+const { TaskData } = require('task-data')
+
+const tasks = []
+const taskData = new TaskData(tasks)
+const taskLogic = new TaskLogic(taskData)
+
+const router = Router()
+
+const jsonBodyParser = bodyParser.json()
+
+router.post('/tasks', jsonBodyParser, (req, res) => {
+    const { body: { title, description } } = req
+
+    try {
+        taskLogic.create(title, description)
+
+        res.json(success())
+    } catch (err) {
+        res.json(fail(err))
+    }
+})
+
+router.get('/tasks/:status?', (req, res) => {
+    const { params : { status }} = req
+
+    switch(status) {
+        case 'TODO':
+            return res.json(success(compress(taskLogic.listTodos())))
+        case 'DOING':
+            return res.json(success(compress(taskLogic.listDoings())))
+        case 'REVIEW':
+            return res.json(success(compress(taskLogic.listReviews())))
+        case 'DONE':
+            return res.json(success(compress(taskLogic.listDones())))
+        default:
+            res.json(success(compress(taskLogic.list())))
+    }
+})
+
+function compress(tasks) {
+    return tasks.map(({ id, title, status }) => ({ id, title, status }))
+}
+
+router.put('/tasks/:id/:status', (req, res) => {
+    const { params : { id, status }} = req
+
+    try {
+        switch(status) {
+            case 'TODO':
+                taskLogic.markTodo(id)
+                break
+            case 'DOING':
+                taskLogic.markDoing(id)
+                break
+            case 'REVIEW':
+                taskLogic.markReview(id)
+                break
+            case 'DONE':
+                taskLogic.markDone(id)
+                break
+        }
+
+        res.json(success())
+    } catch(err) {
+        res.json(fail(err.message))
+    }
+})
+
+router.delete('/tasks/:id', (req, res) => {
+    const { params : { id }} = req
+
+    try {
+        taskLogic.remove(id)
+
+        res.json(success())
+    } catch(err) {
+        res.json(fail(err.message))
+    }
+})
+
+module.exports = router
