3ce5864aea9c353e2ed3bfc9a0479987263b4efb
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-03 21:40:57 +0100

    :fire: jasmineCli staff/vic/user-api

--- staff/vic/jasmine-users-api.v2.0.0/src/jasmineCLI.spec.js
+++ staff/vic/jasmine-users-api.v2.0.0/src/jasmineCLI.spec.js
@@ -1,4 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const userRouter = require('./api/userRouter')
+
+const app = express()
+
 const Jasmine = require('jasmine')
+
 var jasmine = new Jasmine();
 
 jasmine.loadConfigFile(process.env.JASMINE_CONFIG_PATH);
@@ -11,6 +20,13 @@ jasmine.execute();
 jasmine.onComplete(function (passed) {
     if (passed) {
         console.log('All specs have passed');
+
+        app.use('/api', userRouter)
+
+        const port = process.env.PORT
+
+        app.listen(port, () => console.log(`Users API running on port ${port}`))
+
     } else {
         throw new Error('Specs failed...')
     }
