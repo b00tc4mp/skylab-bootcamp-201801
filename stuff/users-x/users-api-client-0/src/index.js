8c7f6c21a8adb2640830e57306e36ff497d7b58c
manuelbarzi <manuelbarzi@gmail.com>
2018-03-07 15:48:15 +0100

    update users stuff

--- stuff/users-x/users-api-client-0/src/index.js
+++ stuff/users-x/users-api-client-0/src/index.js
@@ -1,7 +1,5 @@
 const rp = require('request-promise')
 
-// TODO implement api client
-
 const api = {
     _baseUrl() {
         with (this) {
@@ -10,8 +8,6 @@ const api = {
     },
 
     _call(method, path, body) {
-        // return rp(`${this._baseUrl()}/${path}`)
-        //     .then(res => JSON.parse(res))
         return rp({
             method,
             uri: `${this._baseUrl()}/${path}`,
@@ -24,8 +20,20 @@ const api = {
         return this._call('get', 'users')
     },
 
-    create(name, surname, email, username, password) {
+    register(name, surname, email, username, password) {
         return this._call('post', 'user', { name, surname, email, username, password })
+    },
+
+    remove(id, username, password) {
+        return this._call('delete', `user/${id}`, { username, password })
+    },
+
+    retrieve(id) {
+        return this._call('get', `user/${id}`)
+    },
+
+    update(id, name, surname, email, newUsername, newPassword, username, password) {
+        return this._call('put', `user/${id}`, { name, surname, email, newUsername, newPassword, username, password })
     }
 }
 
---
+++ stuff/users-x/users-app-client-0/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import api from 'users-api-client-0'
+
+api.protocol = 'http'
+api.host = 'localhost'
+api.port = '5000'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = { data: [] }
+  }
+
+  componentDidMount() {
+    api.list()
+      .then(({ data }) => {
+        this.setState({ data })
+      })
+      .catch(console.error)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ul>
+          {this.state.data.map(user => <li key={user.id}>{`${user.name} ${user.surname}`}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
