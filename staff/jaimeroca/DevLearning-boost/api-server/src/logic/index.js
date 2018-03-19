6f0fe57ababa2975b0c761305250ef60b2b59bae
manuelbarzi <manuelbarzi@gmail.com>
2018-03-19 13:04:26 +0100

    update dev learning boost

---
+++ staff/jaimeroca/DevLearning-boost/api-server/src/logic/index.js
@@ -0,0 +1,24 @@
+const { User, Course } = require('../models')
+
+const logic = {
+    //It will create a user
+    createUser(name, surname, email, username, password) {
+        return Promise.resolve()
+            .then(() => {
+                return User.findOne({ username })
+            })
+            .then(user => {
+                if (user) throw Error('username already exists')
+
+                return User.create({ name, surname, email, username, password })
+            })
+            .then(({ _id }) => _id)
+    },
+
+    //It will list all courses from the selected language
+    listCoursesByLanguage(language) {
+        return Course.find({ language })
+    }
+}
+
+module.exports = logic
