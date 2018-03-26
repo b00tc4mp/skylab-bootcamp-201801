0f563b9391a45d0cf36ffcafaf064852c540b731
manuelbarzi <manuelbarzi@gmail.com>
2018-03-26 13:36:45 +0200

    boost book buddies

--- staff/asier/book-boodies-boost/react-app-bookbuddies/src/api/bookBuddiesApi.js
+++
@@ -1,8 +0,0 @@
-const booksBuddiesApi = require('api-client')
-
-booksBuddiesApi.protocol = process.env.REACT_APP_API_PROTOCOL
-booksBuddiesApi.host = process.env.REACT_APP_API_HOST
-booksBuddiesApi.port = process.env.REACT_APP_API_PORT
-
-
-module.exports = booksBuddiesApi
